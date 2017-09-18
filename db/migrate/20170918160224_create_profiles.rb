3ecbe0812316b0df1e6c1030188bcc458f9bb5e8
Adam Romano <manoadamro.imp@gmail.com>
2017-09-18 17:03:54 +0100

    set up relationship between profile and user

--- db/migrate/20170918160224_create_profiles.rb
+++ db/migrate/20170918160224_create_profiles.rb
@@ -1,13 +1,13 @@
 class CreateProfiles < ActiveRecord::Migration[5.1]
   def change
     create_table :profiles do |t|
-      t.integer :user_id
       t.string :first_name
       t.string :last_name
       t.string :bio
       t.string :interests
       t.string :location
       t.string :birthday
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918142023) do
+ActiveRecord::Schema.define(version: 20170918160224) do
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
@@ -19,15 +19,16 @@ ActiveRecord::Schema.define(version: 20170918142023) do
   end
 
   create_table "profiles", force: :cascade do |t|
-    t.integer "user_id"
     t.string "first_name"
     t.string "last_name"
     t.string "bio"
     t.string "interests"
     t.string "location"
     t.string "birthday"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
