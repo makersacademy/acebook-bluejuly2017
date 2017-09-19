e170570e57a2fb0b2bd3ebf6330d37c2b9f98587
David Divilly <d.divilly@gmail.com>
2017-09-19 11:43:55 +0100

    more refactoring

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -6,6 +6,7 @@ RSpec.feature "Timeline", type: :feature do
   before(:each) do
     visit "/posts"
     add_message('Message 1')
+    @time1 = format_time_and_date(Time.now.utc)
   end
 
   scenario "Can submit posts and view them" do
@@ -13,15 +14,13 @@ RSpec.feature "Timeline", type: :feature do
   end
 
   scenario "Expect post to have timestamp" do
-    time = format_time_and_date(Time.now.utc)
-    expect(page).to have_content("#{time}")
+    expect(page).to have_content("#{@time1}")
   end
 
   scenario "Posts are in reverse order" do
-    time1 = format_time_and_date(Time.now.utc)
     add_message('Message 2')
     time2 = format_time_and_date(Time.now.utc)
-    message1_string = "Message 1, created at #{time1}"
+    message1_string = "Message 1, created at #{@time1}"
     message2_string = "Message 2, created at #{time2}"
     expect(page).to have_content("#{message2_string} #{message1_string}")
   end
