8aa650d0cd37a939c78225114f706887e3824556
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-09-20 16:28:23 +0100

    reciprocal defriending added

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -12,20 +12,24 @@ class FriendshipsController < ApplicationController
 
     @friendship1 = current_user.friendships.build(:friend_id => params[:friend_id])
 
-    # @friendship2 = User.all.find(params[:friend_id]).friendships.build(:friend_id => current_user)
+    @friendship2 = User.find(params[:friend_id]).friendships.build(:friend_id => current_user.id)
 
     if !friendship_exists && !friending_self && @friendship1.save && @friendship2.save
       flash[:notice] = "Added friend."
-      redirect_to root_url
+      redirect_to users_url
     else
       flash[:notice] = "Unable to add friend."
-      redirect_to root_url
+      redirect_to users_url
     end
   end
 
   def destroy
+    @friend_id = current_user.friendships.find(params[:id]).friend_id
     @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
+    @friendship2 = Friendship.find_by(user_id: @friend_id, friend_id: current_user.id)
+    @friendship2.destroy
+
     flash[:notice] = "Removed friendship."
     redirect_to users_url
   end
