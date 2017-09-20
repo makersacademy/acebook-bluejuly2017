2a234903183fec5e82d7293af57ee060b61d6a9d
Adam Romano <manoadamro.imp@gmail.com>
2017-09-20 14:40:57 +0100

    fixed issues from previous pull request

--- spec/features/clearance/visitor_signs_up_spec.rb
+++ spec/features/clearance/visitor_signs_up_spec.rb
@@ -11,19 +11,18 @@ RSpec.feature "Visitor signs up" do
   end
 
   scenario "with valid email and password" do
-    sign_up_with "valid@example.com", "password", "Michael", "I am Michael", "01/01/1970", "yoga, crack", "Earth"
+    sign_up_with "valid@example.com", "password"
 
     expect_user_to_be_signed_in
   end
 
   scenario "tries with invalid email" do
-    sign_up_with "invalid_email", "password", "Michael", "I am Michael", "01/01/1970", "yoga, crack", "Earth"
-
+    sign_up_with "invalid_email", "password"
     expect_user_to_be_signed_out
   end
 
   scenario "tries with blank password" do
-    sign_up_with "valid@example.com", "", "Michael", "I am Michael", "01/01/1970", "yoga, crack", "Earth"
+    sign_up_with "valid@example.com", ""
 
     expect_user_to_be_signed_out
   end
