e5011473f14e3e84533a4dd5119e3f7ea0785ff5
Abi Travers <atravers4007@gmail.com>
2017-09-20 16:06:22 +0100

    Most tests passing now

--- spec/views/friends/show.html.erb_spec.rb
+++ spec/views/friends/show.html.erb_spec.rb
@@ -3,14 +3,12 @@ require 'rails_helper'
 RSpec.describe "friends/show", type: :view do
   before(:each) do
     @friend = assign(:friend, Friend.create!(
-      :title => "Title",
       :body => "MyText"
     ))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Title/)
     expect(rendered).to match(/MyText/)
   end
 end
