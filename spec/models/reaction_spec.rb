d60c9183a8352e649ad301de046029cfeb4144d3
Olivia Frost <olivia.frost617@googlemail.com>
2017-09-20 15:07:08 +0100

    need to change branch

--- spec/models/reaction_spec.rb
+++ spec/models/reaction_spec.rb
@@ -4,5 +4,10 @@ RSpec.describe Reaction, type: :model do
   it { is_expected.to be }
 end
 
+Rspec.describe Reaction, type: :model do
 
+  it 'can add rows to the database' do
+    Reaction.create
+  end
   
+end
