308913b6fe270134a5fc4fca795cf4bace2e59dc
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-09-20 11:49:59 +0100

    user and frienship controllers added, frienship db created, migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :friendships
   resources :posts
+  resources :users
 
   root to: redirect('/posts')
+
 end
