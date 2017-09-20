8b2af33e735f140f3f74335d32ba89eaac4e8448
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-09-20 15:08:16 +0100

    no duplicate friendships or self adds

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,29 @@
 class FriendshipsController < ApplicationController
+
+  def create
+
+    def friendship_exists
+      current_user.friendships.exists?(friend_id: params[:friend_id])
+    end
+
+    def friending_self
+      current_user.id.to_s == params[:friend_id].to_s
+    end
+
+    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+    if !friendship_exists && !friending_self && @friendship.save
+      flash[:notice] = "Added friend."
+      redirect_to root_url
+    else
+      flash[:notice] = "Unable to add friend."
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    @friendship = current_user.friendships.find(params[:id])
+    @friendship.destroy
+    flash[:notice] = "Removed friendship."
+    redirect_to users_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
-class UsersController < ApplicationController
-end
+class UsersController < Clearance::UsersController
+
+    def index
+      @users = User.all
+
+      @friendships = current_user.friendships
+
+    end
+
+    private
+
+    def post_params
+      params.require(:user).permit(:message)
+    end
+  end
