96187d7ee2fd84869910d3ce650d67d759c1543c
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-09-20 12:18:56 +0100

    rubocop corrections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
-
   def show
     @user = current_user
     # @posts_sorted = Post.all.sort_by { |post| post.created_at }.reverse
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   resources :posts
 
-
   get '/users/:id/profile', to: 'users#show'
 
   root to: redirect('/posts')
