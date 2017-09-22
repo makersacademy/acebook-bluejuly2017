3c3270f56f524acc2bff629959b529fc9e8f249d
stephengeller <stephen.geller@hotmail.com>
2017-09-22 12:01:02 +0100

    added schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,13 @@ ActiveRecord::Schema.define(version: 20170919140334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "photos", force: :cascade do |t|
     t.text "description"
     t.string "image"
